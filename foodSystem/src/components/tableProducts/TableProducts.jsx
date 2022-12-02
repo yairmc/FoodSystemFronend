64a7a4f3fcc767c8e63d35cbbf5d10119466d776
Orlando Camacho <soy.orlandocg@gmail.com>
2022-12-02 11:22:48 -0700

    fix

--- foodSystem/src/components/tableProducts/TableProducts.jsx
+++ foodSystem/src/components/tableProducts/TableProducts.jsx
@@ -1,54 +1,211 @@
+
+import style from "./TableProduct.module.css";
+import IMG_DELETE from "/img/deleteIcon.svg";
+import IMG_UPDATE from "/img/editIcon.svg";
+
 const TableProducts = () => {
-    return (
+     return (
         <>
-        <br></br>
-            <table>
-                <thead>
+            <table className={style.containerTable}>
+                <thead className={style.headTable}>
                     <tr>
-                        <th>
-                            Prepared Product
+                        <th className={style.titleTable}>
+                            Packaged Product
                         </th>
-                        <th>
+                        <th className={style.titleTable}>
                             Cost
                         </th>
-                        <th>
+                        <th className={style.titleTable}>
                             Base Price
                         </th>
-                        <th>
+                        <th className={style.titleTable}>
                             Taxes
                         </th>
-                        <th>
+                        <th className={style.titleTable}>
                             Stock
                         </th>
+                        <th className={style.titleTable}>
+                            Edit
+                        </th>
+                        <th className={style.titleTable}>
+                            Delete
+                        </th>
                     </tr>
+
                 </thead>
-                <tbody id="packagedArea"></tbody>
-            </table>
-            <br></br>
-            <table>
-                <thead>
+                <tbody className={style.bodyTable}>
                     <tr>
                         <th>
-                            Prepared Product
+                            Pastel
                         </th>
                         <th>
-                            Cost
+                            50
                         </th>
                         <th>
-                            Base Price
+                            50
                         </th>
                         <th>
-                            Taxes
+                            50
                         </th>
                         <th>
-                            Ingredients
+                            50
+                        </th>
+                        <th>
+                            <img src={IMG_UPDATE} alt="" />
+                        </th>
+                        <th>
+                            <img src={IMG_DELETE} alt="" />
                         </th>
                     </tr>
+                    <tr>
+                        <th>
+                            Donas
+                        </th>
+                        <th>
+                            60
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            <img src={IMG_UPDATE} alt="" />
+                        </th>
+                        <th>
+                            <img src={IMG_DELETE} alt="" />
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>
+                            Pay
+                        </th>
+                        <th>
+                            22
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            <img src={IMG_UPDATE} alt="" />
+                        </th>
+                        <th>
+                            <img src={IMG_DELETE} alt="" />
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+            <br></br>
+            <table className={style.containerTable}>
+                <thead className={style.headTable}>
+                    <tr>
+                        <th className={style.titleTable}>
+                            Prepared Product
+                        </th>
+                        <th className={style.titleTable}>
+                            Cost
+                        </th>
+                        <th className={style.titleTable}>
+                            Base Price
+                        </th>
+                        <th className={style.titleTable}>
+                            Taxes
+                        </th>
+                        <th className={style.titleTable}>
+                            Ingredients
+                        </th>
+                        <th className={style.titleTable}>
+                            Edit
+                        </th>
+                        <th className={style.titleTable}>
+                            Delete
+                        </th>
+                    </tr>
+
                 </thead>
-                <tbody id="preparedArea"></tbody>
+                <tbody className={style.bodyTable}>
+                    <tr>
+                        <th>
+                            Pastel
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            ingredient, ingredient
+                        </th>
+                        <th>
+                            <img src={IMG_UPDATE} alt="" />
+                        </th>
+                        <th>
+                            <img src={IMG_DELETE} alt="" />
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>
+                            Donas
+                        </th>
+                        <th>
+                            60
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                        ingredient, ingredient
+                        </th>
+                        <th>
+                            <img src={IMG_UPDATE} alt="" />
+                        </th>
+                        <th>
+                            <img src={IMG_DELETE} alt="" />
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>
+                            Pay
+                        </th>
+                        <th>
+                            22
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                            50
+                        </th>
+                        <th>
+                        ingredient, ingredient
+                        </th>
+                        <th>
+                            <img src={IMG_UPDATE} alt="" />
+                        </th>
+                        <th>
+                            <img src={IMG_DELETE} alt="" />
+                        </th>
+                    </tr>
+                </tbody>
             </table>
         </>
-        
     )
 }
 
